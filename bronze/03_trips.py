494d4fb8794e4b3c64ff5fa5697defb19b861dc9
Felipe Santos <felipe.dossantos@agrisolus.com.br>
2022-10-17 11:34:22 -0300

    fact_trips

--- bronze/03_trips.py
+++ bronze/03_trips.py
@@ -8,12 +8,12 @@ df = spark.read.format("csv").option("sep", ",").load("/FileStore/raw_data/trips
 
 df = df.withColumn("trip_id", df._c0)
 df = df.withColumn("rideable_type", df._c1)
-df = df.withColumn("start_at", df._c2.cast("timestamp"))
-df = df.withColumn("ended_at", df._c3.cast("timestamp"))
+df = df.withColumn("start_at", df._c2)
+df = df.withColumn("ended_at", df._c3)
 
 df = df.withColumn("start_station_id", df._c4)
 df = df.withColumn("end_station_id", df._c5)
-df = df.withColumn("rider_id", df._c6.cast("int"))
+df = df.withColumn("rider_id", df._c6)
 
 columns_to_drop = ["_c0", "_c1", "_c2", "_c3", "_c4", "_c5", "_c6"]
 df = df.drop(*columns_to_drop)
